f4e8cab32e7e6a6d41fd36a1304dbf4241ab51e5
AmikaMehta123 <amikamehta701@gmail.com>
2023-11-27 15:19:39 -0800

    changed app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-
 import "./components/topbar/topbar.css";
 import "./app.css";
 import Home from "./pages/Home";
-import "./components/topbar/topbar.css"
-import "./App.css"
-import Home from "./pages/Home"
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-
 import UserList from "./pages/userList/UserList";
 import Schedule from "./pages/schedule/Schedule";
 import MaintainanceTeam from "./pages/maintainanceTeam/MaintainanceTeam";
 import Alerts from "./pages/alerts/Alerts";
 import EditUser from "./pages/editUser/EditUser";
-import AddCamera from "./pages/addcamera/AddCamera";
-import UpdateCamera from "./pages/updateCamera/UpdateCamera";
-import ViewCamera from "./pages/viewcameras/ViewCamera";
-import DeleteCamera from "./pages/deleteCamera/deleteCamera";
-function App() {
+import Help from "./pages/help/Help";
+import Faq from "./pages/Faq/Faq";
+import CameraCatlog from "./pages/cameraCatlog/CameraCatlog";
+import Billing from "./pages/billing/Billing";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import TestingCamera from "./pages/testingCamera/TestingCamera";
+import { ShopContextProvider } from "./context/shopContext";
+import MaintainDashboard from "./pages/maintainDashboard/MaintainDashboard";
+
+function Content() {
+  const location = useLocation();
+  const isAuthPage = location.pathname === "/login" || location.pathname === "/signup";
+  const showTopbarAndSidebar = !isAuthPage;
+
   return (
     <>
       {showTopbarAndSidebar &&( 
@@ -65,3 +68,4 @@ function App() {
 }
 
 export default App;
+
