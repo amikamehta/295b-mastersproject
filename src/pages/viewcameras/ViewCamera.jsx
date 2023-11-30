e7cf00f00964c2f9dfeaffe7435899b7f87d4c79
ed <edventura91@gmail.com>
2023-11-30 11:05:40 -0800

    changes to view

--- src/pages/viewcameras/ViewCamera.jsx
+++ src/pages/viewcameras/ViewCamera.jsx
@@ -7,50 +7,36 @@ export default function ViewCamera() {
       <section class="grid-container">
 
   <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
+    <iframe width="560" height="315"  src = "http://127.0.0.1:5000/video_feed?ipaddress=http://webcam.town.karuizawa.nagano.jp/krcam07/mjpg/1/video.mjpg" frameborder = "0" allowfullscreen></iframe>
     <button class="button1">ID 1</button>
     <button class="button2">edit</button>
   </div>
   <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="http://127.0.0.1:5000/video_feed?ipaddress=http://webcam.town.karuizawa.nagano.jp/krcam02/mjpg/video.mjpg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     <button class="button1">ID 2</button>
     <button class="button2">edit</button>
   </div>
   <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="http://127.0.0.1:5000/video_feed?ipaddress=http://mbr-cam.dyndns.org:8088/mjpg/video.mjpg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     <button class="button1">ID 3</button>
     <button class="button2">edit</button>
   </div>
   <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="http://127.0.0.1:5000/video_feed?ipaddress=http://mmb.aa1.netvolante.jp:1025/mjpg/video.mjpg?resolution=640x360&compression=50" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     <button class="button1">ID 4</button>
     <button class="button2">edit</button>
   </div>
   <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="http://127.0.0.1:5000/video_feed?ipaddress=http://camera6.buffalotrace.com/mjpg/video.mjpg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     <button class="button1">ID 5</button>
     <button class="button2">edit</button>
   </div>
   <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
+  <iframe width="560" height="315" src="http://127.0.0.1:5000/video_feed?ipaddress=http://camera.buffalotrace.com/mjpg/video.mjpg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     <button class="button1">ID 6</button>
     <button class="button2">edit</button>
   </div>
-  <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
-    <button class="button1">ID 7</button>
-    <button class="button2">edit</button>
-  </div>
-  <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
-    <button class="button1">ID 8</button>
-    <button class="button2">edit</button>
-  </div>
-  <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
-    <button class="button1">ID 9</button>
-    <button class="button2">edit</button>
-  </div>
+
 
   
 </section>
