a18edd70fc727694d349756d56e6489216d40923
AmikaMehta123 <amikamehta701@gmail.com>
2023-11-30 10:25:54 -0800

    updating change

--- src/pages/viewcameras/ViewCamera.jsx
+++ src/pages/viewcameras/ViewCamera.jsx
@@ -7,7 +7,8 @@ export default function ViewCamera() {
       <section class="grid-container">
 
   <div class="grid-item">
-    <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe>
+    {/* <iframe src = "https://www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder = "0" allowfullscreen></iframe> */}
+    <iframe src = "http://127.0.0.1:5000/video_feed?ipaddress=http://webcam.town.karuizawa.nagano.jp/krcam07/mjpg/1/video.mjpg" frameborder = "0" allowfullscreen></iframe>
     <button class="button1">ID 1</button>
     <button class="button2">edit</button>
   </div>
